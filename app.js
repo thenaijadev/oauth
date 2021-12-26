6d91ea7b8fe7f5ef8547cef9f7ae5cfddf75022a
Okwor Chukwuemeka <thenaijadev@gmail.com>
2021-12-26 10:04:22 -0800

    salting level 5

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const passport = require("passport");
+
 // const mongodb = require("mongodb");
 
 app.use(bodyParser.urlencoded({
@@ -23,10 +26,6 @@ const userSchema = new mongoose.Schema({
 
 const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt, {
-    secret: secret,
-    encryptedFields: ["password"]
-});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -46,23 +45,30 @@ app.get("/register", function (req, res) {
 
 
 app.post("/register", function (req, res) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
 
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        User.findOne({
+            email: req.body.username
+        }, function (err, foundUser) {
+            if (!foundUser) {
+                newUser.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Saved successfully");
+                        res.render("secrets");
+                    }
+                });
+            } else {
+                res.send(`${foundUser.email} already exist`);
+            }
+        })
 
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
     });
-
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Saved successfully");
-            res.render("secrets");
-        }
-    })
-
-
 });
 
 app.post("/login", function (req, res) {
@@ -75,9 +81,13 @@ app.post("/login", function (req, res) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                });
+
+
             }
         }
     });
