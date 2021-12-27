3372dc75c50c9766918bd92f614acd53cef018d2
Okwor Chukwuemeka <thenaijadev@gmail.com>
2021-12-27 11:31:07 -0800

    completed oauth

--- app.js
+++ app.js
@@ -1,88 +1,158 @@
 //jshint esversion:6
-require("dotenv").config();
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const app = express();
 const mongoose = require("mongoose");
-const session = require("express-session");
+const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
-// const mongodb = require("mongodb");
+const app = express();
 
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.set("view engine", "ejs");
-app.use(express.static("public"));
 
-//initialisation of the session package.
 app.use(session({
-    secret: "code is the love of my life.",
+    secret: "Our little secret.",
     resave: false,
-    saveUinitialized: false,
-    cookie: {
-        maxAge: 60 * 1000 * 60 * 3
-    },
-
+    saveUninitialized: false
 }));
-
-
-//initialisation of passport.
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
-//mongoose setup is below.
-mongoose.connect("mongodb://localhost:27017/userDB",
-    err => {
-        if (err) throw err;
-        console.log('connected to MongoDB')
-    });
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true
+});
 
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
-//initialization of passport-local mongoose.
-userSchema.plugin(passportLocalMongoose)
 const User = new mongoose.model("User", userSchema);
-const secret = process.env.SECRET;
+
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
 
-//Definition of the routes is below.
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+
+        User.findOrCreate({
+            googleId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
 app.get("/", function (req, res) {
     res.render("home");
 });
 
+app.get("/auth/google",
+    passport.authenticate('google', {
+        scope: ["profile"]
+    })
+);
+
+app.get("/auth/google/secrets",
+    passport.authenticate('google', {
+        failureRedirect: "/login"
+    }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect("/secrets");
+    });
+
 app.get("/login", function (req, res) {
     res.render("login");
 });
-
 
 app.get("/register", function (req, res) {
     res.render("register");
 });
 
 app.get("/secrets", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
-    } else {
-        res.redirect("/login")
-    }
+    User.find({
+        "secret": {
+            $ne: null
+        }
+    }, function (err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {
+                    usersWithSecrets: foundUsers
+                });
+            }
+        }
+    });
+});
 
-})
+app.get("/submit", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret;
+
+    //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+    // console.log(req.user.id);
+
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
 app.post("/register", function (req, res) {
+
     User.register({
         username: req.body.username
     }, req.body.password, function (err, user) {
@@ -91,17 +161,20 @@ app.post("/register", function (req, res) {
             res.redirect("/register");
         } else {
             passport.authenticate("local")(req, res, function () {
-                res.redirect("/secrets")
+                res.redirect("/secrets");
             });
         }
     });
+
 });
 
 app.post("/login", function (req, res) {
+
     const user = new User({
         username: req.body.username,
-        password: req.body.passport
+        password: req.body.password
     });
+
     req.login(user, function (err) {
         if (err) {
             console.log(err);
@@ -110,13 +183,16 @@ app.post("/login", function (req, res) {
                 res.redirect("/secrets");
             });
         }
-    })
-});
-app.get("/logout", function () {
-    req.logout();
-    res.redirect("/");
+    });
+
 });
 
-app.listen("3000", function () {
-    console.log("Server is started on port 3000");
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000.");
 });
